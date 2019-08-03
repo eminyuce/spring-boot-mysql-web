381dbc419386f904c3292cc2d152cf1e6312d451
EMIN YUCE <eminyuce@gmail.com>
2019-08-03 12:55:07 +0300

    Design Changes

--- src/main/java/i2i/n5g/logs/LogManagerMain.java
+++ src/main/java/i2i/n5g/logs/LogManagerMain.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootMySqlApplication {
+public class LogManagerMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMySqlApplication.class, args);
+		SpringApplication.run(LogManagerMain.class, args);
 	}
 }
