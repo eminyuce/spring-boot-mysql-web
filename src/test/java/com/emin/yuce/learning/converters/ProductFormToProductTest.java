7d1803b14858a2682665359f7aa3a251a13fab03
eminyuce <eminyuce@gmail.com>
2023-09-30 23:37:22 -0400

    ProductFormToProductTest unit tests

---
+++ src/test/java/com/emin/yuce/learning/converters/ProductFormToProductTest.java
@@ -0,0 +1,36 @@
+package com.emin.yuce.learning.converters;
+
+import com.emin.yuce.learning.commands.ProductForm;
+import com.emin.yuce.learning.domain.Product;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProductFormToProductTest {
+
+    private ProductFormToProduct converter = new ProductFormToProduct();
+
+    @Test
+    public void testConvert() {
+        ProductForm productForm = new ProductForm(1, "desc", new BigDecimal(10.0), "url");
+
+        Product product = converter.convert(productForm);
+
+        assertEquals(1, product.getId());
+        assertEquals("desc", product.getDescription());
+        assertEquals(new BigDecimal(10.0), product.getPrice());
+        assertEquals("url", product.getImageUrl());
+    }
+
+    @Test
+    public void testConvertWithEmptyId() {
+        ProductForm productForm = new ProductForm(0, "desc", new BigDecimal(10.0), "url");
+
+        Product product = converter.convert(productForm);
+
+        assertEquals(0, product.getId());
+    }
+
+}
