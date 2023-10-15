b3812fc62c01a7a3b37362a2f9bed121eb913891
eminyuce <eminyuce@gmail.com>
2023-10-14 21:09:33 -0400

    remove product endpoints 2

--- src/main/java/com/emin/yuce/learning/controllers/ProductController.java
+++ src/main/java/com/emin/yuce/learning/controllers/ProductController.java
@@ -23,16 +23,6 @@ public class ProductController {
 
     private ProductToProductForm productToProductForm;
 
-    @Autowired
-    public void setProductToProductForm(ProductToProductForm productToProductForm) {
-        this.productToProductForm = productToProductForm;
-    }
-
-    @Autowired
-    public void setProductService(ProductService productService) {
-        this.productService = productService;
-    }
-
 
     @RequestMapping("/product/show/{id}")
     public String getProduct(@PathVariable String id, Model model) {
