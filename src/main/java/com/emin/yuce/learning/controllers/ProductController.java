edaeabc258f67133d63eda7aad13641197431544
eminyuce <eminyuce@gmail.com>
2023-10-14 21:13:40 -0400

    remove product controller

--- src/main/java/com/emin/yuce/learning/controllers/ProductController.java
+++ src/main/java/com/emin/yuce/learning/controllers/ProductController.java
@@ -19,57 +19,5 @@ import javax.validation.Valid;
  */
 @Controller("/product")
 public class ProductController {
-    private ProductService productService;
 
-    private ProductToProductForm productToProductForm;
-
-    @Autowired
-    public void setProductToProductForm(ProductToProductForm productToProductForm) {
-        this.productToProductForm = productToProductForm;
-    }
-
-    @Autowired
-    public void setProductService(ProductService productService) {
-        this.productService = productService;
-    }
-
-
-    @RequestMapping("/product/show/{id}")
-    public String getProduct(@PathVariable String id, Model model) {
-        model.addAttribute("product", productService.getById(Integer.valueOf(id)));
-        return "product/show";
-    }
-
-    @RequestMapping("product/edit/{id}")
-    public String edit(@PathVariable String id, Model model) {
-        Product product = productService.getById(Integer.valueOf(id));
-        ProductForm productForm = productToProductForm.convert(product);
-
-        model.addAttribute("productForm", productForm);
-        return "product/productform";
-    }
-
-    @RequestMapping("/product/new")
-    public String newProduct(Model model) {
-        model.addAttribute("productForm", new ProductForm());
-        return "product/productform";
-    }
-
-    @RequestMapping(value = "/product", method = RequestMethod.POST)
-    public String saveOrUpdateProduct(@Valid ProductForm productForm, BindingResult bindingResult) {
-
-        if (bindingResult.hasErrors()) {
-            return "product/productform";
-        }
-
-        Product savedProduct = productService.saveOrUpdateProductForm(productForm);
-
-        return "redirect:/product/show/" + savedProduct.getId();
-    }
-
-    @RequestMapping("/product/delete/{id}")
-    public String delete(@PathVariable String id) {
-        productService.delete(Integer.valueOf(id));
-        return "redirect:/product/list";
-    }
 }
